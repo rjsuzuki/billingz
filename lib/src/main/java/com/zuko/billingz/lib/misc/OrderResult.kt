159c53d6241c411bb523dd6bbce96d9a04f1e70d
rjsuzuki <ryanjsuzuki@gmail.com>
2021-09-17 10:44:11 -0700

    remove: delete deprecated module

--- lib/src/main/java/com/zuko/billingz/lib/misc/OrderResult.kt
+++
@@ -1,3 +0,0 @@
-package com.zuko.billingz.lib.misc
-
-class OrderResult
--- lib/src/main/java/com/zuko/billingz/lib/misc/Response.kt
+++
@@ -1,9 +0,0 @@
-package com.zuko.billingz.lib.misc
-
-data class Response(
-
-    var id: Int?,
-    var status: Status
-) {
-    enum class Status
-}
