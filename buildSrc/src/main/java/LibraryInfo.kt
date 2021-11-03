2eb2555e8340c89234efe933221d35e6ac862fa6
rjsuzuki <ryanjsuzuki@gmail.com>
2021-11-03 13:13:28 -0700

    fix: bump SDK to 30 and downgrade core-ktx to 1.6.0 to resolve build error.

--- buildSrc/src/main/java/LibraryInfo.kt
+++ buildSrc/src/main/java/LibraryInfo.kt
@@ -1,8 +1,8 @@
 object LibraryInfo {
 
-    const val compileSDK = 29
+    const val compileSDK = 30
     const val buildTools = "31.0.0"
     const val minSDK = 21
-    const val targetSDK = 29
+    const val targetSDK = 30
 
 }
