9678a3a89b85497536993d6fa8f3a23b5fc2ce80
rjsuzuki <ryanjsuzuki@gmail.com>
2022-03-18 12:42:59 -0700

    change: update all module gradle files and dependencies to their latest versions. Google Billing library is now v4.1.0

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,23 @@
+object Dependencies {
+
+    const val androidx_core = "1.7.0"
+    const val androidx_constraint_layout = "2.1.3"
+    const val androidx_appcompat = "1.4.1"
+    const val androidx_lifecycle_java8 = "2.4.1"
+
+    const val google_material = "1.5.0"
+    const val google_billing = "4.1.0"
+
+    const val lottie = "4.2.0"
+
+    const val amazon_iap = "2.0.76"
+
+    const val kotlin = "1.6.10"
+    const val kotlin_coroutines = "1.6.0-native-m"
+
+    const val junit = "4.13.2"
+    const val androidx_junit = "1.1.3"
+    const val espresso_core = "3.4.0"
+}
+
+
